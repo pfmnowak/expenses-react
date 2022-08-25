f937ec4e2fef1728c3507732c0bd7c3e53d51520
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-08-25 14:18:41 +0200

    Organize components, switch to arrow functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+// function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -29,6 +30,6 @@ function App() {
       <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import Card from "./Card";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       {props.expenses.map((e) => (
@@ -10,6 +10,6 @@ function Expenses(props) {
       ))}
     </Card>
   );
-}
+};
 
 export default Expenses;
