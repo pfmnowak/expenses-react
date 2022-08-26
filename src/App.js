5be62e2587398f0150ea408ddac7c7375f5ded96
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-08-26 23:41:31 +0200

    Add a form for new expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 // function App() {
 const App = () => {
@@ -26,7 +27,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
